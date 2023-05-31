09e201e57986d7e3b91ab40e84c0c568b4f932d7
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2023-05-31 10:39:28 -0400

    Added event dectection method

--- Main/src/main.rs
+++ Main/src/main.rs
@@ -1,4 +1,5 @@
-use bevy::{prelude::*,};
+
+use bevy::{prelude::*};
 use bevy::{app::App, DefaultPlugins};
 
 #[derive(Component)]
@@ -9,15 +10,27 @@ struct Background;
 
 #[derive(Component, Clone)]
 pub struct Player{
-    pub animation: i8
+    pub animation: i8,
+    pub gaming: bool,
+    pub positionx: f32,
+    pub positiony: f32,
+    pub carryingBox: bool,
+    pub interacting: bool
 }
 
 #[derive(Component)]
 pub struct Boxes{
     // if boxType = true, Box is banana; this will be loaded accordingly
-    pub boxType: bool,
-    pub boxValue: i32,
-    pub inPlay: bool
+    pub box_type: bool,
+    pub box_value: i32,
+    pub in_play: bool,
+    pub speed: f32,
+    pub picked_up: bool,
+    pub laying: bool
+}
+#[derive(Component)]
+struct text{
+    value:i8
 }
 
 fn main() {
@@ -40,6 +53,7 @@ fn main() {
         .add_system(bevy::window::close_on_esc)
         .add_system(tick)
         .add_system(boxMovement)
+        .add_system(pickupBoxes)
         .run();
 }
 
@@ -63,19 +77,26 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
     commands
         .spawn( SpriteBundle {
         visibility: Visibility::Visible,
-        transform: Transform {translation: Vec3::new(-600., 100., 1.), scale: Vec3::new(0.25, 0.25, 1.), rotation: Quat::from_rotation_z(3. * 1.570796)},
+        transform: Transform {translation: Vec3::new(-600., 100., 2.), scale: Vec3::new(0.25, 0.25, 1.), rotation: Quat::from_rotation_z(3. * 1.570796)},
         texture: asset_server.load("C:/Programming/Python/Rust/Gather/projectAssets/person.png"),
         ..default()
         })
         .insert(Player{
-            animation: 0
+            animation: 0,
+            gaming: true,
+            positionx: -600.,
+            positiony: 100.,
+            carryingBox: false,
+            interacting: false
         });
 
         let mut texturePath: &str = "";
         let mut boxType = false;
+        let mut inPlay = true;
         for boxnum in 1..21{
             if boxnum % 2 == 0 {
                 boxType = true;
+                inPlay = false;
                 texturePath = "C:/Programming/Python/Rust/Gather/projectAssets/banana.png";
             } else {
                 boxType = false;
@@ -88,51 +109,125 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
                 ..default()
             })
             .insert(Boxes{
-                boxType: boxType,
-                boxValue: boxnum,
-                inPlay: false
+                box_type: boxType,
+                box_value: boxnum,
+                in_play: inPlay,
+                speed: 1.,
+                picked_up: false,
+                laying: false
             });
         }
+
 
 }
 
 fn tick(
     time: Res<Time>, 
     mut char_evr : EventReader<ReceivedCharacter>,
-    mut query: Query<(&Player, &mut Transform, Entity)>,
+    mut query: Query<(&mut Player, &mut Transform)>,
     mut commands: Commands,
 ){
-    let (player, mut transform, entity) = query.single_mut();
 
-    for event in char_evr.iter() {
-        // player movement
-        if event.char == 'w'{
-            transform.translation.y += 300. * time.delta_seconds();
+    let (mut player, mut transform) = query.single_mut();
 
-        } else if event.char == 's'{
-            transform.translation.y -= 300. * time.delta_seconds();
-        }
+    player.positionx = transform.translation.x;
+    player.positiony = transform.translation.y;
 
-        if event.char == 'd' {
-            transform.translation.x += 300. * time.delta_seconds();
-        } else if event.char == 'a' {
-            transform.translation.x -= 300. * time.delta_seconds();
-        }
-
-        
-    } 
+    // Todo, Create logic for gaming = false to end game
+    if char_evr.is_empty() == true{
+        player.interacting = false;
+    } else {
+        for event in char_evr.iter() {
+            // player interaction handler + code above
+            if event.char == 'e'{
+                player.interacting = true;
+            }
+    
+            // player movement
+            // Todo, remove frame skip issue
+            if event.char == 'w'{
+                transform.translation.y += 300. * time.delta_seconds();
+    
+            } else if event.char == 's'{
+                transform.translation.y -= 300. * time.delta_seconds();
+            }
+    
+            if event.char == 'd' {
+                transform.translation.x += 300. * time.delta_seconds();
+            } else if event.char == 'a' {
+                transform.translation.x -= 300. * time.delta_seconds();
+            }
+    
+           
+    
+            
+        } 
+    
+    }
 }   
 
 
 fn boxMovement(
     time: Res<Time>,
-    mut query: Query<(&Boxes, &mut Transform)>
+    mut query: Query<(&mut Boxes, &mut Transform)>,
+    mut playerQuery: Query<(&mut Player)>
 ){
-    for boxy in query.iter(){
-        // this will iterate through each boxe that was rendered, this will also let us move certain boxes
-        println!("{} is inplay: {}", boxy.0.boxValue, boxy.0.inPlay)
-        if boxy.0.inPlay == true{
-            
+
+    query.for_each_mut( |mut boxx| {
+
+        let (mut boxy, mut transform) = boxx;
+        
+        if boxy.in_play == true && boxy.laying == false{
+            if boxy.picked_up == true{
+
+            } else {
+                // move the box down the conveyor
+                if transform.translation.y > 283.{
+                    transform.translation.y -= 10. * time.delta_seconds();
+
+                // if the box has gone off the screen game over. Set the players struct to turn off the game
+                } else if transform.translation.x > 677.{
+                    let mut player = playerQuery.single_mut();
+                    player.gaming = false;
+
+                // move the box accross the conveyor
+                } else {
+                    transform.translation.x += 10. * time.delta_seconds();
+                }
+            }
+
+        } else {
+            transform.translation.z = -1.
         }
+
+
     }
+    );
+}
+
+
+fn pickupBoxes(
+    mut playerQuery: Query<&mut Player>,
+    mut boxQuery: Query<(&mut Boxes, &mut Transform)>
+){
+            //         // if  (player - box).abs().floor() < 10; player is in range, this must be compared for both x and y
+    let mut player: Mut<Player> = playerQuery.single_mut();
+    if player.interacting == true{
+        boxQuery.for_each_mut(|mut boxx|{
+            if (player.positionx - boxx.1.translation.x).abs().floor() < 25. && (player.positiony - boxx.1.translation.y).abs().floor() < 25.{
+                if player.carryingBox == false {
+                    boxx.0.picked_up = true;
+                    player.carryingBox = true;
+                    boxx.0.laying = false;
+                } else {
+                    boxx.0.picked_up = false;
+                    player.carryingBox = false;
+                    boxx.0.laying = true;
+                }
+
+            }
+            // else {}
+        })
+    }
+    
 }
